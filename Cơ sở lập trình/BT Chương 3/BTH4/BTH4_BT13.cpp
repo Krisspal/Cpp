1ecb62a12e4003898a4ce0506a07a1438718bdbd
Krisspal <69229024+Krisspal@users.noreply.github.com>
2021-12-21 17:51:29 +0700

    Update BTH4_BT13.cpp

--- Cơ sở lập trình/BT Chương 3/BTH4/BTH4_BT13.cpp
+++ Cơ sở lập trình/BT Chương 3/BTH4/BTH4_BT13.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int main()
 {	
 	//Khai bao bien
-	double n, max = INT_MIN, min = INT_MAX, sum = 0, i = 1;
-	string EXIT = "CTRL + Z va Enter";
+	double n, max = INT_MIN, min = INT_MAX, sum = 0, i = 1;	//max va min lay gia tri MAX de khong bo sot n 
+	string EXIT = "CTRL + Z va Enter";	//Khai bao bien de thay doi cho de
 	
 	//Nhap so, tinh tong va tim max, min
 	cout << "Nhap so nguyen hoac nhan " << EXIT <<" de ket thuc\n";
-	while (!cin.eof())		//EOF == END OF FILE
+	
+	while (!cin.eof())		//EOF == END OF FILE == Ket thuc nhap khi nhan CTRL + Z
 	{
 		cout << "Nhap so thu " << i << ": ", cin >> n;
 		sum = sum + n;
