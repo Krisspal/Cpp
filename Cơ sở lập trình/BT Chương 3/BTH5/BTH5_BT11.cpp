d7f99d96e44e2cd35960dc8df9abf864a8c0772c
Krisspal <69229024+Krisspal@users.noreply.github.com>
2021-12-21 09:47:39 +0700

    Update BTH5_BT11.cpp

--- Cơ sở lập trình/BT Chương 3/BTH5/BTH5_BT11.cpp
+++ Cơ sở lập trình/BT Chương 3/BTH5/BTH5_BT11.cpp
@@ -13,28 +13,28 @@ int main()
 			cout << "Nhap sai! Vui long nhap lai!\n";
 	} while (c <= 0);
 	cout << "a) Hinh vuong dac\n";
-		for (int i = 1; i <= c; i++)
-		{
-			for (int j = 1; j <= c; j++)
-				cout << " *";
-				cout << endl;
-		}
-	cout << "\nb) Hinh vuong rong\n";
-		for (int i = 1; i <= c; i++)
-		{
+	for (int i = 1; i <= c; i++)
+	{
+		for (int j = 1; j <= c; j++)
 			cout << " *";
-			for (int j = 1; j <= c; j++)
+			cout << endl;
+	}
+	cout << "\nb) Hinh vuong rong\n";
+	for (int i = 1; i <= c; i++)
+	{
+		for (int j = 1; j <= c; j++)
+		{
+			if (j == 1 || j == c || i == 1 || i == c)
 			{
-				if (j == 1 || j == c || i == 1 || i == c)
-				{
-				cout << " *";
-				cout << endl;
-				}
-
-				else
-					cout << " ";
+				cout << "*";
 			}
+
+			else
+				cout << " ";
+			}
+			cout <<endl;
 		}
-		system("pause");
-		return 0;
+	cout <<endl;
+	system("pause");
+	return 0;
 }
