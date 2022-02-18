96918a08e22423beb52edd9b98585fb966129090
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-02-18 09:00:31 +0700

    Update BTH8_BT02.cpp

--- Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT02.cpp
+++ Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT02.cpp
@@ -1,28 +1,27 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main()
-//{
-//	const int MAXSIZE = 100;
-//	int n;
-//	double values[MAXSIZE];
-//	//Cau d
-//	cout << "Nhap vao so phan tu n:\n";
-//	//Cau e
-//	cin >> n;
-//	cout << "Nhap gia tri cac phan tu:\n";
-//	for (int i = 0; i < n; i++)
-//		cin >> values[i];
-//	//Cau f
-//	cout << "Xuat mang:\n";
-//	for (int i = 0; i < n; i++)
-//	{
-//		cout << values[i];
-//		if (i < n - 1)
-//			cout << ", ";
-//	}
-//	cout << endl;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int MAXSIZE = 100;
+	int n;
+	double values[MAXSIZE];
+	//Cau d
+	cout << "Nhap vao so phan tu n:\n";
+	//Cau e
+	cin >> n;
+	cout << "Nhap gia tri cac phan tu:\n";
+	for (int i = 0; i < n; i++)
+		cin >> values[i];
+  //Cau f
+	cout << "Xuat mang:\n";
+	for (int i = 0; i < n; i++
+  {
+		cout << values[i];
+		if (i < n - 1)
+			cout << ", "
+  }
+	cout << endl;
+	system("pause");
+	return 0;
+}
