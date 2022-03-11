b887a3d7a7345cc7626135d3582cee716214fdd6
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-03-11 14:39:27 +0700

    Create BTH3_BT2.cpp

---
+++ Kỹ thuật lập trình/BTH3/BTH3_BT2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+	int *a,n,*p;
+	do
+	{
+		cout<<"Nhap so phan tu: ";
+		cin>>n;
+		if(n<=0 || n>30 )
+			cout<<"Nhap sai"<<endl;
+	}while(n<=0 || n>30 );
+	a =new int [n];
+	for(int i=0;i<n;i++)
+	{
+		a[i]= rand()%10;
+	}
+	p=a;
+	cout <<endl;
+	cout<<"xuat mang do doi"<<endl;
+	for(int i=0;i<n;i++)
+		cout<<" "<<*(p+i);
+	cout<<endl;
+	cout<<"xuat mang don chi so"<<endl;
+	for(int i=0;i<n;i++)
+		cout<<" "<<p[i];
+	cout<<endl;
+	delete []a;
+	a=0;
+	system("pause");
+	return 0;
+
+}
