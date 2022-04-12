99c6d363fec4c8f194c19089309cccfdf204c289
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-04-12 09:37:57 +0700

    Update BTH8_BT11.cpp

--- Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT11.cpp
+++ Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT11.cpp
@@ -1,60 +1,60 @@
-//#include <iostream>
-//#include <iomanip>
-//#include <cstdlib>
-//
-//using namespace std;
-//
-//void TANG(int a[], int n)
-//{
-//	for (int i = 0; i < n; i++)
-//	{
-//		for (int j = i + 1 ; j < n; j++)
-//		{
-//			if (a[i] > a[j])
-//			{
-//				int tam = a[i];
-//				a[i] = a[j];
-//				a[j] = tam;
-//			}
-//			
-//		}
-//		
-//	}
-//}
-//
-//const int MAXSIZE = 100;
-//
-//int main()
-//{
-//	int a[MAXSIZE], n;
-//	cout << "Nhap kich thuoc mang\n";
-//	cin >> n;
-//	cout << "Nhap phan tu mang\n";
-//	for (int i = 0; i < n; i++)
-//	{
-//		/*cin >> a[i];*/
-//		a[i] = rand() % 100;
-//	}
-//	
-//	
-//	cout << "Xuat mang\n";
-//	for (int i = 0; i < n; i++)
-//	{
-//		cout << setw(9) << a[i];
-//		if (i % 10 == 0 && i != 0)
-//			cout << endl;
-//	}
-//	cout << "\nSap xet cac phan tu tang dan\n";
-//	
-//	TANG(a, n);
-//	
-//	for (int i = 0; i < n; i++)
-//	{
-//		cout << setw(9) << a[i];
-//		if (i % 10 == 0 && i != 0)
-//			cout << endl;
-//	}
-//	cout << endl;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+void TANG(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1 ; j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				int tam = a[i];
+				a[i] = a[j];
+				a[j] = tam;
+			}
+			
+		}
+		
+	}
+}
+
+const int MAXSIZE = 100;
+
+int main()
+{
+	int a[MAXSIZE], n;
+	cout << "Nhap kich thuoc mang\n";
+	cin >> n;
+	cout << "Nhap phan tu mang\n";
+	for (int i = 0; i < n; i++)
+	{
+		/*cin >> a[i];*/
+		a[i] = rand() % 100;
+	}
+	
+	
+	cout << "Xuat mang\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << setw(9) << a[i];
+		if (i % 10 == 0 && i != 0)
+			cout << endl;
+	}
+	cout << "\nSap xet cac phan tu tang dan\n";
+	
+	TANG(a, n);
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << setw(9) << a[i];
+		if (i % 10 == 0 && i != 0)
+			cout << endl;
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
