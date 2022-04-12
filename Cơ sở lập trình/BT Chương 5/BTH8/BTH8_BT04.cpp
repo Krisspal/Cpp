996606c6b9b4117441f636cfb6e349eb4f3bde88
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-04-12 09:34:37 +0700

    Update BTH8_BT04.cpp

--- Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT04.cpp
+++ Cơ sở lập trình/BT Chương 5/BTH8/BTH8_BT04.cpp
@@ -1,41 +1,41 @@
-//#include <iostream>
-//#include <iomanip>
-//#include <cstdlib>
-//
-//using namespace std;
-//
-//const int MAXSIZE = 100;
-//int main()
-//{
-//	double n, tong = 0;
-//	double a[MAXSIZE];
-//
-//	cout << "Nhap so phan tu n:\n";
-//	cin >> n;
-//
-//	cout << "Nhap cac phan tu:\n";
-//	for (int i = 0; i < n; i++)
-//	/*{
-//		cout << "Phan tu " << i << ": ";
-//		cin >> a[i];
-//	}*/
-//	a[i] = rand() % 100;
-//
-//	//Xuat mang
-//	cout << "Cac phan tu trong mang:\n";
-//	for (int i = 0; i < n; i++)
-//		if (i % 10 == 0)
-//			cout << endl;
-//		cout << setw(9) << a[i];
-//
-//	cout << endl;
-//	//Tinh tong
-//	for (int i = 0; i < n; i++)
-//		tong += a[i];
-//
-//	cout << "Tong cac phan tu trong mang la " << tong << endl;
-//	cout << "Trung binh cong cac phan tu trong mang la " << (double)tong / n << endl;
-//
-//	system("pause");
-//	return 0;
-//}
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+const int MAXSIZE = 100;
+int main()
+{
+	double n, tong = 0;
+	double a[MAXSIZE];
+
+	cout << "Nhap so phan tu n:\n";
+	cin >> n;
+
+	cout << "Nhap cac phan tu:\n";
+	for (int i = 0; i < n; i++)
+	/*{
+		cout << "Phan tu " << i << ": ";
+		cin >> a[i];
+	}*/
+	a[i] = rand() % 100;
+
+	//Xuat mang
+	cout << "Cac phan tu trong mang:\n";
+	for (int i = 0; i < n; i++)
+		if (i % 10 == 0)
+			cout << endl;
+		cout << setw(9) << a[i];
+
+	cout << endl;
+	//Tinh tong
+	for (int i = 0; i < n; i++)
+		tong += a[i];
+
+	cout << "Tong cac phan tu trong mang la " << tong << endl;
+	cout << "Trung binh cong cac phan tu trong mang la " << (double)tong / n << endl;
+
+	system("pause");
+	return 0;
+}
