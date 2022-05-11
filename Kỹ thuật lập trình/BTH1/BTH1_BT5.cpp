a238141ad89108c4441d5fe9f5555b3ef31ee793
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-05-11 11:07:21 +0700

    Update BTH1_BT5.cpp

--- Kỹ thuật lập trình/BTH1/BTH1_BT5.cpp
+++ Kỹ thuật lập trình/BTH1/BTH1_BT5.cpp
@@ -1,53 +1,66 @@
+//Update using pointer
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-
 using namespace std;
-const int M = 10, N = 10;
+
 int main()
 {
-	int a[M][N], n, tong = 0;
-
-	//Nhap so dong va cot
-	do
+	int** p, n, tong1 = 0, tong2 = 0;
+	cout << "Nhap cap cua ma tran vuong\n";
+	cin >> n;
+	//Cap phat mang
+	p = new int*[n];
+	for (int i = 0; i < n; i++)
 	{
-		cout << "Nhap cap cua ma tran vuong (0 < n <= 10)\n";
-		cin >> n;
-		if (n > 10 || n <= 0)
-			cout << "Nhap sai! Vui long hhap lai\n";
-	} while (n > 10 || n <= 0);
+		p[i] = new int[n];
+	}
 	//Tao mang
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			a[i][j] = rand() % 100;
+			p[i][j] = rand() % 100;
 		}
 	}
 	//Xuat mang
-	cout << "Xuat mang:\n";
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			cout << setw(3) << a[i][j];
-			cout << " ";
+			cout << setw(9) << p[i][j];
 		}
 		cout << endl;
 	}
 	//Tinh tong
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) 
 	{
 		for (int j = 0; j < n; j++)
 		{
 			if (i == j)
-			{	
-				tong += a[i][j];
+			{
+				tong1 += p[i][j];
 			}
-				
 		}
 	}
-	cout << "Tong cac gia tri tren duong cheo chinh la " << tong << endl;
-
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			{
+				if (i + j == n - 1)
+				tong2 += p[i][j];
+			}
+		}
+	}
+	cout << "Tong cac phan tu tren duong cheo chinh la " << tong1 << endl;
+	cout << "Tong cac phan tu tren duong cheo phu la " << tong2 << endl;
+	//Huy cap phat
+	for (int i = 0; i < n; i++)
+	{
+		delete[]p[i];
+	}
+	delete[]p;
+	p = 0;
 	return 0;
 }
