3f4a1645a8d4eb494b643105e4205a93fc978c34
LeXunAnh <101849769+LeXunAnh@users.noreply.github.com>
2022-05-23 16:17:34 +0700

    Update BTH4_BT9.cpp
    bổ sung

--- Kỹ thuật lập trình/BTH4: Chuỗi/BTH4_BT9.cpp
+++ Kỹ thuật lập trình/BTH4: Chuỗi/BTH4_BT9.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void HO(char* hoten, int n)
 {
-	int tachho;
+	int tachho=0;
 	for (int i = 0; i < n; i++)
 	{
 		if (isspace(hoten[i]))
@@ -20,4 +20,57 @@ void HO(char* hoten, int n)
 		cout << hoten[j];
 	}
 	cout << endl;
+}
+void TEN(char* hoten, int n)
+{
+	int tachten=0;
+	for (int i = n - 1; i > 0; i--)
+	{
+		if (isspace(hoten[i]))
+		{
+			tachten = i;
+			break;
+		}
+	}
+	cout << "Ten:";
+	for (int j = tachten; j < n; j++)
+	{
+		cout << hoten[j];
+	}
+}
+void TENLOT(char* hoten, int n)
+{
+	int ten=0,ho=0,tenlot=0;
+	for (int i = 0; i < n; i++)
+	{
+		if(isspace(hoten[i]))
+		{ 
+			ten = i;
+		}
+	}
+	for (int i = n - 1; i > 0; i--)
+	{
+		if (isspace(hoten[i]))
+		{
+			ho = i;
+		}
+	}
+	cout << "Ten lot: ";
+	for (int j = ho + 1; j < ten; j++)
+	{
+		cout << hoten[j];
+	}
+	cout << endl;
+}
+int main() 
+{
+	int n;
+	char HOTEN[50];
+	cout << "Nhap ho va ten\n";
+	cin.getline(HOTEN, 50);
+	n = strlen(HOTEN);
+	HO(HOTEN,n);
+	TENLOT(HOTEN, n);
+	TEN(HOTEN, n);
+	return 0;
 }
