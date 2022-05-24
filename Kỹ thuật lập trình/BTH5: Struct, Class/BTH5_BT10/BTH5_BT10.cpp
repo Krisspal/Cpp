b3048b2c0b2a8d0ee6d53eda0a2a54d7033380b2
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-05-24 09:32:24 +0700

    Update BTH5_BT10.cpp

--- Kỹ thuật lập trình/BTH5: Struct, Class/BTH5_BT10/BTH5_BT10.cpp
+++ Kỹ thuật lập trình/BTH5: Struct, Class/BTH5_BT10/BTH5_BT10.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
 #include "GPG.h"
+
+using namespace std;
+void XUATTHOIGIAN(GPG T)
+{
+	cout << T.getgio() << ":" << T.getphut() << ":" <<T.getgiay() << endl;
+}
+
 int main()
 {
-	GPG T1, T2, kqKC;
-	int a1, b1, c1, a2, b2, c2, K=0;
-	cout << "Nhap thoi diem thu nhat: ";
-	cin >> a1 >> b1 >> c1;
-	cout << "Nhap thoi diem thu hai: ";
-	cin >> a2 >> b2 >> c2;
-	T1.setGPG(a1, b1, c1);
-	T2.setGPG(a2, b2, c2);
-	K=abs(T1.getgio()*3600+T1.getphut()*60+T1.getgiay()-(T2.getgio()*3600+T2.getphut()*60+T2.getgiay()));
-	kqKC.setGPG(K/3600, (K%3600)/60, K%60);
-	cout << "Khoang cach giua hai thoi diem la: " << kqKC.getgio() << " gio " << kqKC.getphut() << " phut " << kqKC.getgiay() << " giay \n";
-	system("pause");
+	GPG T1, T2, KC;
+	int s1, m1, h1,
+		s2, m2, h2,
+		khoangcach;
+	
+	cout << "Nhap thoi diem thu nhat (gio:phut:giay):\n";
+	cin >> h1 >> m1 >> s1;
+	cout << "Nhap thoi diem thu hai (gio:phut:giay):\n";
+	cin >> h2 >> m2 >> s2;
+	T1.setGPG(h1, m1, s1);
+	T2.setGPG(h2, m2, s2);
+	
+	cout << "Thoi diem 1: ";
+	XUATTHOIGIAN(T1);
+	cout << "Thoi diem 2: ";
+	XUATTHOIGIAN(T2);
+	
+	khoangcach = abs(T1.getgio() * 3600 + T1.getphut() * 60 + T1.getgiay() - (T2.getgio() * 3600 + T2.getphut() * 60 + T2.getgiay())); //Tinh khoang cach theo giay, them tri tuyet doi (abs) de loai truong hop am.
+	KC.setGPG(khoangcach / 3600, (khoangcach % 3600) / 60, khoangcach % 60);
+	cout << "Khoang cach giua 2 thoi diem la ";
+	XUATTHOIGIAN(KC);
 	return 0;
 }
