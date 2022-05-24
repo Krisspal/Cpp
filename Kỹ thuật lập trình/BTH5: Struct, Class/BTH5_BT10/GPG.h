dd4fab5668ddf27634f5cd2e8ff24c280b8b3320
Krisspal <69229024+Krisspal@users.noreply.github.com>
2022-05-24 09:34:17 +0700

    Update GPG.h

--- Kỹ thuật lập trình/BTH5: Struct, Class/BTH5_BT10/GPG.h
+++ Kỹ thuật lập trình/BTH5: Struct, Class/BTH5_BT10/GPG.h
@@ -6,6 +6,9 @@ class GPG
 private:
 	int gio, phut, giay;
 public:
+	/*Inline functions (hàm nội tuyến) là một loại hàm trong ngôn ngữ lập trình C++. 
+	Từ khoá inline được sử dụng để đề nghị (không phải là bắt buộc) compiler (trình biên dịch) thực hiện inline expansion (khai triển nội tuyến) với hàm đó.
+	Hay nói cách khác là chèn code của hàm đó tại địa chỉ mà nó được gọi.*/
 	inline int getgio();
 	inline int getphut();
 	inline int getgiay();
