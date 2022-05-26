76c4da1cbc5e1bf2abc452f5870614fef2c4788c
LeXunAnh <101849769+LeXunAnh@users.noreply.github.com>
2022-05-26 23:12:35 +0700

    Update BTH6_BT6.cpp

--- Kỹ thuật lập trình/BTH6: Làm việc với tập tin/BTH6_BT6.cpp
+++ Kỹ thuật lập trình/BTH6: Làm việc với tập tin/BTH6_BT6.cpp
@@ -5,16 +5,30 @@ using namespace std;
 
 int main()
 {
-	ifstream Xuat("D:/BT4.txt");
+	ifstream Xuat;
+	ofstream NhapBT6;
 	char ch;
-	while (Xuat)
+	Xuat.open("E:/VS FILE/TEXT/BT4.txt", ios::in);
+	NhapBT6.open("E:/VS FILE/TEXT/BT6.txt", ios::out);
+	if (Xuat.is_open())
 	{
-		Xuat.get(ch);
-		if (ch == 'U' || ch == 'E' || ch == ' O ' || ch == 'A' || ch == 'I')
+		if (NhapBT6.is_open())
 		{
-			 cout << ch;
+			while (Xuat)
+			{
+				Xuat >> ch;
+				if (ch == 'A' || ch == 'I' || ch == 'U' || ch == 'O' || ch == 'E')
+				{
+					NhapBT6 << ch;
+					NhapBT6 << " ";
+				}
+			}
+			NhapBT6.close();
 		}
+		cout << "Ghi thanh cong!";
+		Xuat.close();
 	}
-	Xuat.close();
+	else
+		cout << "Ghi khong thanh cong!";
 	return 0;
 }
